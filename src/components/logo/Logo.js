139a98251d3bb723d5d5e043c116daca72d0205e
Bender85 <nikitosik85@gmail.com>
2018-02-18 19:49:40 +0200

    created app

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Logo.scss';
+
+class Logo extends Component {
+    render() {
+        return (
+            <header className="logo">
+                its the future app logo!!!
+            </header>
+        )
+    }
+}
+
+export default Logo;
