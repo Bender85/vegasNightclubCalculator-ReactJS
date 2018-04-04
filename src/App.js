26070431f8fe5fd57c4d5fcfa0c29fd7a6ed3d60
Bender85 <nikitosik85@gmail.com>
2018-04-04 12:25:49 +0300

    remove unuse components and added two configured files

--- src/App.js
+++ src/App.js
@@ -3,23 +3,12 @@ import logo from './logo.svg';
 import './App.scss';
 
 import  Logo from './components/logo/Logo';
-import  Calculation from './components/calculator/Calculation';
-import  Compare from './components/compare/Compare';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/*<header className="App-header">*/}
-          {/*<img src={logo} className="App-logo" alt="logo" />*/}
-          {/*<h1 className="App-title">Welcome to React</h1>*/}
-        {/*</header>*/}
-        {/*<p className="App-intro">*/}
-          {/*To get started, edit <code>src/App.js</code> and save to reload.*/}
-        {/*</p>*/}
           <Logo/>
-          <Calculation/>
-          <Compare/>
       </div>
     );
   }
