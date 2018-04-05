f8656930a642cb94693bacc1f98d65046ad046ec
Bender85 <nikitosik85@gmail.com>
2018-04-05 17:52:04 +0300

    add redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './App.scss';
 
 import  Logo from './components/logo/Logo';
+import  load from './utils/load';
 import SelectClub from './components/selectClub/selectClub';
 
+
+// import reducers from './reducers';
+// const store = createStore(reducers);
+const red = () => {};
+const store = createStore(red);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    // Устанавливаем состояние
+    this.state = {
+      data: null,
+      // active: 0,
+      // term: ''
+    };
+    // Сразу загружаем данные
+    this.loadData();
+  }
+
+  loadData() {
+    load('https://swapi.co/api/planets/').then(users => {
+      this.setState({
+        data: JSON.parse(users)
+      });
+    });
+  }
+
   render() {
+    console.log(this.state.data);
+    console.log(store);
     return (
-      <div className="App">
-        <Logo/>
-        <div className='calculator'>
-          <div className='flex__container'>
-            <div className="flex__container--item-left">
-              <SelectClub/>
+      <Provider store={store}>
+        <div className="App">
+          <Logo/>
+          <div className='calculator'>
+            <div className='flex__container'>
+              <div className="flex__container--item-left">
+                <SelectClub/>
+              </div>
+
+              <div className="flex__container--item-right footy-calculator__recepit"></div>
             </div>
-
-            <div className="flex__container--item-rightfooty-calculator__recepit"></div>
           </div>
-        </div>
 
-        <div className="compare footy-compare"></div>
-      </div>
+          <div className="compare footy-compare"></div>
+        </div>
+      </Provider>
     );
   }
 }
--- src/components/selectClub/selectClub.js
+++ src/components/selectClub/selectClub.js
@@ -1,40 +1,11 @@
 import React, { Component } from 'react';
-
-import data from '../../clubs.json';
-
+import { connect } from 'react-redux';
 
 
 class SelectClub extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    // this.state = {items: []}
-  }
-  handleChange = (e) => {
-    let val = e.target.value;
-    this.setState({ value: val });
-  };
-  // componentWillMount() {
-  //   fetch('http://www.softomate.net/ext/employees/list.json')
-  //     .then(response => response.json())
-  //     .then(({result: items}) => this.setState({items}))
-  // }
-
   render() {
-    let items = this.state.value;
-    console.log(items);
-    let _data = [];
-    for(let i = 0; i < data.length; i++) {
-      _data.push(data[i].club)
-    }
     return (
-      <select name="" id="" value={this.state.value} onChange={this.handleChange}>
-        {
-          _data[0].map(function(item, i){
-            return <option key={i} value={item.name}>{item.name}</option>;
-          })
-        }
-      </select>
+      <h1>left side</h1>
     );
   }
 }
